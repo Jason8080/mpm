82e04dc13ee761b0590fa2c8b75b9e7ec4ba430d
Jason.lee <gmlee.boss@gmail.com>
2020-05-31 20:09:39 +0800

    准备签功能

--- mpm-bpm/src/main/java/com/gm/mpm/bpm/business/BusinessFlow.java
+++ mpm-bpm/src/main/java/com/gm/mpm/bpm/business/BusinessFlow.java
@@ -2,6 +2,7 @@ package com.gm.mpm.bpm.business;
 
 import com.gm.mpm.bpm.business.recorder.PauseRecorder;
 import com.gm.mpm.bpm.business.recorder.ReplyRecorder;
+import com.gm.mpm.bpm.flow.Flow;
 import com.gm.mpm.bpm.node.Node;
 
 import java.util.List;
@@ -19,17 +20,18 @@ public interface BusinessFlow<F> {
      *
      * @param initiator 发起人
      * @param template  流程模板
-     * @return 返回发起节点
+     * @return 返回发起节点 flow
      */
-    Node initiate(Long initiator, Long template);
+    Flow initiate(Long initiator, Long template);
 
     /**
      * 关闭流程.
      *
+     * @param id       流程ID
      * @param executor 执行人
      * @param remark   备注
      */
-    void close(Long executor, String remark);
+    void close(Long id, long executor, String remark);
 
     /**
      * 流程结束.
@@ -48,7 +50,14 @@ public interface BusinessFlow<F> {
     List<ReplyRecorder> replyRecorder();
 
     /**
-     * 暂存快照.
+     * 回复.
+     *
+     * @param replyRecorder the reply recorder
+     */
+    void reply(ReplyRecorder replyRecorder);
+
+    /**
+     * 获取暂存记录.
      *
      * <p>
      * 用于支撑随意暂存执行.
@@ -57,4 +66,12 @@ public interface BusinessFlow<F> {
      * @return the long
      */
     List<PauseRecorder> pauseRecorder();
+
+
+    /**
+     * 暂存.
+     *
+     * @param pauseRecorder the pause recorder
+     */
+    void pause(PauseRecorder pauseRecorder);
 }
