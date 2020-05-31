6336530db80cc29eb7553c532f0a910618cc01e2
Jason.lee <gmlee.boss@gmail.com>
2020-05-31 14:59:23 +0800

    设计流程节点, 提供快照记录执行记录 .

--- mpm-bpm/src/main/java/com/gm/mpm/bpm/node/Node.java
+++ mpm-bpm/src/main/java/com/gm/mpm/bpm/node/Node.java
@@ -1,8 +1,6 @@
 package com.gm.mpm.bpm.node;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * 流程节点接口
@@ -27,7 +25,7 @@ public interface Node {
      * 节点的唯一标识
      * </p>
      *
-     * @return 默认UUID生成
+     * @return 默认UUID生成 id
      */
     default Long getId() {
         return UUID.randomUUID().getMostSignificantBits();
@@ -48,7 +46,7 @@ public interface Node {
      * 60. 协同节点: 需要处理表单内容
      * </p>
      *
-     * @return 默认为空节点
+     * @return 默认为空节点 type
      */
     default Integer getType() {
         return 0;
@@ -60,11 +58,18 @@ public interface Node {
      * 0. 串形: 当前节点是串发节点
      * 1. 并形: 当前节点是并发节点
      *
-     * @return 默认串形
+     * @return 默认串形 form
      */
     default Integer getForm() {
         return SERIAL_NODE_FORM;
     }
+
+    /**
+     * Sets form.
+     *
+     * @param form the form
+     */
+    void setForm(Integer form);
 
 
     /**
@@ -74,9 +79,9 @@ public interface Node {
      * 可用于返回上一节点和下一节点
      * </p>
      *
-     * @return 默认自己.
+     * @return 默认自己. initial node
      */
-    default Long getInitiator() {
+    default Long getInitialNode() {
         return getId();
     }
 
@@ -87,11 +92,18 @@ public interface Node {
      * 下一节点可能是并发节点
      * </p>
      *
-     * @return 返回下一节点数组
+     * @return 返回下一节点数组 long [ ]
      */
     default Long[] nextIds() {
-        return new Long[]{getInitiator()};
+        return new Long[]{getInitialNode()};
     }
+
+    /**
+     * Sets next ids.
+     *
+     * @param ids the ids
+     */
+    void setNextIds(Long[] ids);
 
     /**
      * 获取上一节点.
@@ -100,36 +112,86 @@ public interface Node {
      * 上一节点可能是并发节点
      * </p>
      *
-     * @return 上一节点数组
+     * @return 上一节点数组 long [ ]
      */
     default Long[] prevIds() {
-        return new Long[]{getInitiator()};
+        return new Long[]{getInitialNode()};
     }
+
+    /**
+     * Sets prev ids.
+     *
+     * @param ids the ids
+     */
+    void setPrevIds(Long[] ids);
 
 
     /**
      * 必填的字段.
      *
      * <p>
-     *     该数据用于协同节点: 必须处理某内容才能提交 (进入下一节点)
+     * 该数据用于协同节点: 必须处理某内容才能提交 (进入下一节点)
      * </p>
      *
-     * @return 返回必填的字段, 如果对必填字段的值有要求, 可存至value, 没有要求统一存null.
+     * @return 返回必填的字段 : {  如果对必填字段的值有要求, 可存至value, 没有要求统一存null.  另外快照存储时value为真实提交的Value. }
      */
     default Map<String,Object> requiredFields(){
         return new HashMap(0);
     }
 
     /**
-     * 当前节点执行记录.
+     * Sets required fields.
+     *
+     * @param requiredFields the required fields
+     */
+    void setRequiredFields(Map<String, Object> requiredFields);
+
+    /**
+     * 节点执行时间: 执行才有 .
+     *
+     * @return 返回节点处理时间. long
+     */
+    default Long executionTime(){
+        return System.currentTimeMillis();
+    }
+
+    /**
+     * Sets execution time.
+     *
+     * @param executionTime the execution time
+     */
+    void setExecutionTime(Long executionTime);
+
+    /**
+     * 执行人.
      *
      * <p>
-     *     用于支撑随意跳跃指定节点执行
+     * 可能是部门单位. 不止是个人
      * </p>
      *
-     * @return 返回当前节点的执行记录.
+     * @return 返回执行人ID long
      */
-    default Void recorder(){
-        return null;
+    default Long executor(){
+        return 0L;
+    }
+
+    /**
+     * Sets executor.
+     *
+     * @param executor the executor
+     */
+    void setExecutor(Long executor);
+
+    /**
+     * 当前节点执行快照.
+     *
+     * <p>
+     * 用于支撑随意跳跃指定节点执行
+     * </p>
+     *
+     * @return 返回当前节点的执行记录. list
+     */
+    default List<NodeSnapshot> snapshots(){
+        return new ArrayList();
     }
 }
---
+++ mpm-bpm/src/main/java/com/gm/mpm/bpm/node/NodeSnapshot.java
@@ -0,0 +1,23 @@
+package com.gm.mpm.bpm.node;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * 流程节点执行记录器.
+ * <p>
+ *     该记录器主要用于支撑流程跳跃执行.
+ * </p>
+ *
+ * @author Jason
+ */
+public interface NodeSnapshot extends Node {
+    /**
+     * 执行记录编号.
+     *
+     * @return 返回编号.
+     */
+    default Long getRecorderId(){
+        return UUID.randomUUID().getLeastSignificantBits();
+    }
+}
