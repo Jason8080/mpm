12e19e807e251ac5c2e7f670e5c29dde96791128
Jason <xiaoku13141@163.com>
2020-07-04 19:11:27 +0800

    待集成mybatis.

--- mpm-data/src/main/java/com/gm/mpm/data/config/MybatisConfig.java
+++ mpm-data/src/main/java/com/gm/mpm/data/config/MybatisConfig.java
@@ -1,9 +1,27 @@
 package com.gm.mpm.data.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 @MapperScan("com.gm.mpm.data.mapper")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MybatisConfig {
+
+
+
+    @Primary
+    @Bean("master")
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource master() {
+        return DataSourceBuilder.create().build();
+    }
 }
