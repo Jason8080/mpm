516162499e8e5a11e67601f655f30e5f603c53d8
Jason <xiaoku13141@163.com>
2020-07-04 19:29:09 +0800

    完成动态数据源配置 .

--- mpm-data/src/main/java/com/gm/mpm/data/config/MybatisConfig.java
+++ mpm-data/src/main/java/com/gm/mpm/data/config/MybatisConfig.java
@@ -1,5 +1,7 @@
 package com.gm.mpm.data.config;
 
+import com.gm.mpm.data.config.dynamic.DynamicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,20 +10,60 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @MapperScan("com.gm.mpm.data.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MybatisConfig {
-
-
-
     @Primary
     @Bean("master")
     @ConfigurationProperties(prefix = "spring.datasource.master")
     public DataSource master() {
         return DataSourceBuilder.create().build();
+    }
+
+
+    @Primary
+    @Bean("slave")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource slave() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("dynamicDataSource")
+    public DataSource dynamicDataSource() {
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        Map<Object, Object> dataSourceMap = new HashMap<>(2);
+        dataSourceMap.put("master", master());
+        dataSourceMap.put("slave", slave());
+        // 将 master 数据源作为默认指定的数据源
+        dynamicDataSource.setDefaultDataSource(master());
+        // 将 master 和 slave 数据源作为指定的数据源
+        dynamicDataSource.setDataSources(dataSourceMap);
+        return dynamicDataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        // 配置数据源，此处配置为关键配置，如果没有将 dynamicDataSource作为数据源则不能实现切换
+        sessionFactory.setDataSource(dynamicDataSource());
+        sessionFactory.setTypeAliasesPackage("com.gm.mpm.**.model");    // 扫描Model
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath*:**/mapper/*.xml"));    // 扫描映射文件
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        // 配置事务管理, 使用事务时在方法头部添加@Transactional注解即可
+        return new DataSourceTransactionManager(dynamicDataSource());
     }
 }
